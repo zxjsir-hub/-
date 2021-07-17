7b0cfded8eba6db46510bf7e978b20f169c6479d
zxjsir <78460796+zxjsir-hub@users.noreply.github.com>
2021-07-17 18:25:41 +0800

    Add files via upload

--- 行政区域查询.py
+++ 行政区域查询.py
@@ -8,11 +8,9 @@ import time
 key = ''  # 高德地图API
 # 只支持单个关键词语搜索关键词支持,行政区名称、citycode、adcode
 
-while True:
-    # keywords = '中国'
+while True:  # 创建循环，如果查询结果成功，跳出循环
     keywords = input('请输入需查询的关键词，例如，搜索国家（中国），搜索省份（例如山东），能够显示市（例如济南），区（例如历下区）:\n')
     # 设置显示下级行政区级数,可选值：0、1、2、3
-    # subdistrict = '3'
     subdistrict = input('设置显示下级行政区级数,输入数字：0、1、2、3：\n')
     # 此项控制行政区信息中返回行政区边界坐标点;base:不返回行政区边界坐标点；all:只返回当前查询district的边界值，不返回子节点的边界值；
     extensions = 'base'
@@ -21,13 +19,13 @@ while True:
     data = r.json()
     # print(data)
     建议结果列表 = data['count']
-    print(建议结果列表)
+    # print(建议结果列表)
     if 建议结果列表 == '0':
         print('参数输入错误，请重新输入！返回值：', 建议结果列表)
     else:
         break
 
-行政区数据列表 = []
+行政区数据列表 = []  # 创建空列表，用户存储查询的数据
 
 s1 = data['districts'][0]['adcode']  # 区域编码
 s2 = data['districts'][0]['name']  # 行政区名称
@@ -47,7 +45,7 @@ if s4 == 'street':
 
 一级行政区数据 = data['districts'][0]['districts']
 一级行政区个数 = len(一级行政区数据)
-print(一级行政区个数)
+#print(一级行政区个数)
 
 for i in range(0, 一级行政区个数):
     s1 = 一级行政区数据[i]['adcode']
